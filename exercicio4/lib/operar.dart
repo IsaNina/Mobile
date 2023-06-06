04b89f89ea48280414f6a412b421bfa7ab63906e
IsabelleOrmo <bellecrenonini@gmail.com>
2023-06-06 09:02:24 -0300

    ex

---
+++ exercicio4/lib/operar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyOperar extends StatefulWidget {
+  const MyOperar({super.key});
+
+  @override
+  State<MyOperar> createState() => _MyOperarState();
+}
+
+class _MyOperarState extends State<MyOperar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
